0a5de62c5b98b425aef9f83299781e8e8b2ab021
Rabbial Anower Home <rabbialrabbi@gmail.com>
2021-11-08 20:21:28 +0600

    Candidate fix: Candidate Contact info update fix (Nullable issue)

--- tests/Feature/CandidateContactInfoUpdateTest.php
+++ tests/Feature/CandidateContactInfoUpdateTest.php
@@ -29,19 +29,6 @@ class CandidateContactInfoUpdateTest extends TestCase
     }
 
     /**
-     * Person email required Test
-     * @return void
-     */
-    public function test_candidate_per_email_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['per_email' => ['The per email field is required.']]);
-    }
-
-    /**
      * Person email length validation Test
      * @return void
      */
@@ -55,19 +42,6 @@ class CandidateContactInfoUpdateTest extends TestCase
 
         $this->response->assertJsonMissing(['per_email' => ['The per email field is required.']]);
         $this->response->assertJsonFragment(['per_email' => ['The per email must not be greater than 255 characters.']]);
-    }
-
-    /**
-     * Person mobile number required Test
-     * @return void
-     */
-    public function test_candidate_mobile_number_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['mobile_number' => ['The mobile number field is required.']]);
     }
 
     /**
@@ -87,19 +61,6 @@ class CandidateContactInfoUpdateTest extends TestCase
     }
 
     /**
-     * Person current residence country required Test
-     * @return void
-     */
-    public function test_candidate_per_current_residence_country_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['per_current_residence_country' => ['The per current residence country field is required.']]);
-    }
-
-    /**
      * Person current residence country length validation Test
      * @return void
      */
@@ -113,19 +74,6 @@ class CandidateContactInfoUpdateTest extends TestCase
 
         $this->response->assertJsonMissing(['per_current_residence_country' => ['The per current residence country field is required.']]);
         $this->response->assertJsonFragment(['per_current_residence_country' => ['The per current residence country must not be greater than 255 characters.']]);
-    }
-
-    /**
-     * Person current residence city required Test
-     * @return void
-     */
-    public function test_candidate_per_current_residence_city_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['per_current_residence_city' => ['The per current residence city field is required.']]);
     }
 
     /**
@@ -145,19 +93,6 @@ class CandidateContactInfoUpdateTest extends TestCase
     }
 
     /**
-     * Person permanent country required Test
-     * @return void
-     */
-    public function test_candidate_per_permanent_country_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['per_permanent_country' => ['The per permanent country field is required.']]);
-    }
-
-    /**
      * Person permanent country length validation Test
      * @return void
      */
@@ -171,19 +106,6 @@ class CandidateContactInfoUpdateTest extends TestCase
 
         $this->response->assertJsonMissing(['per_permanent_country' => ['The per permanent country field is required.']]);
         $this->response->assertJsonFragment(['per_permanent_country' => ['The per permanent country must not be greater than 255 characters.']]);
-    }
-
-    /**
-     * Person permanent city required Test
-     * @return void
-     */
-    public function test_candidate_per_permanent_city_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['per_permanent_city' => ['The per permanent city field is required.']]);
     }
 
     /**
@@ -203,19 +125,6 @@ class CandidateContactInfoUpdateTest extends TestCase
     }
 
     /**
-     * Person county required Test
-     * @return void
-     */
-    public function test_candidate_per_county_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['per_county' => ['The per county field is required.']]);
-    }
-
-    /**
      * Person county length validation Test
      * @return void
      */
@@ -232,19 +141,6 @@ class CandidateContactInfoUpdateTest extends TestCase
     }
 
     /**
-     * Person permanent post code required Test
-     * @return void
-     */
-    public function test_candidate_per_permanent_post_code_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['per_permanent_post_code' => ['The per permanent post code field is required.']]);
-    }
-
-    /**
      * Person permanent post code length validation Test
      * @return void
      */
@@ -258,19 +154,6 @@ class CandidateContactInfoUpdateTest extends TestCase
 
         $this->response->assertJsonMissing(['per_permanent_post_code' => ['The per permanent post code field is required.']]);
         $this->response->assertJsonFragment(['per_permanent_post_code' => ['The per permanent post code must not be greater than 255 characters.']]);
-    }
-
-    /**
-     * Person permanent address required Test
-     * @return void
-     */
-    public function test_candidate_per_permanent_address_required()
-    {
-        $this->response = $this->withHeaders([
-            'Authorization' => 'Bearer' . $this->token
-        ])->POST('/api/v1/candidate/personal-cotactinformation', []);
-
-        $this->response->assertJsonFragment(['per_permanent_address' => ['The per permanent address field is required.']]);
     }
 
     /**
@@ -314,13 +197,12 @@ class CandidateContactInfoUpdateTest extends TestCase
             'per_permanent_post_code' => '1230',
             'per_permanent_address' => 'Dhaka',
         ];
+
         $this->response = $this->withHeaders([
             'Authorization' => 'Bearer' . $this->token
         ])->POST('/api/v1/candidate/personal-cotactinformation', $data);
 
         $this->assertApiSuccess();
         $this->assertEquals($data['mobile_number'], $this->response['data']['contact']['mobile_number']);
-
-
     }
 }
