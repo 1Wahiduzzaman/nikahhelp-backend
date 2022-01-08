126ab4cdc290e7ed191abd949e0839aa81a7d430
Mahmud Hasan Shakkhor <shakkhor@tikweb.dk>
2022-01-08 17:46:53 +0600

    MBT-198 Email template fix

--- resources/views/emails/verifyUser.blade.php
+++ resources/views/emails/verifyUser.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome Email</title>
-    <style>
+    <style type="text/css">
         body {
             margin: 0;
         }
@@ -95,57 +95,67 @@
         }
     </style>
 </head>
-<body>
-<div class="header">
-    <a class="logo"><img src="{{ asset('public/images/ma_logo_white.svg') }}" alt="logo" class="mat-logo" /></a>
+<body style="margin: 0;">
+<div style="background: #522e8e; display: flex; justify-content: center;">
+    <a><img src="{{ asset('public/images/ma_logo_white.svg') }}" alt="logo" style="width: 170px; height: 110px;" /></a>
 </div>
 
-<div class="content-div mx-auto title-text">
-    <div class="w-full">
-        <h2>Verify your email address {{ $user['email'] }}</h2>
+<div style="color: rgb(96 84 84 / 85%); margin: 0 auto; width: 500px;">
+    <div style="width: 100%; margin-top: 30px;">
+        <h2 style="font-size: 20px; color: rgba(0,0,0,.5);">Verify your email address {{ $user['email'] }}</h2>
 
-        <p>Dear [Mr./Ms.] {{ $user['full_name'] }},</p>
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 28px;">Dear [Mr./Ms.] {{ $user['full_name'] }},</p>
 
-        <p>
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 28px;">
             Welcome to Matrimony Assist [20-11-2021 10:40:30 +06]
         </p>
 
-        <p>
-            In order ot validate your account please click the link - <a href="{{ $domain }}/emailVerify/{{ $user->verifyUser->token }}" class="link-text">{{ $domain }}/emailVerify/{{ $user->verifyUser->token }}</a> or press the button below
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 28px;">
+            In order ot validate your account please click the link - <a href="{{ $domain }}/emailVerify/{{ $user->verifyUser->token }}" style="color: #1d68a7;">{{ $domain }}/emailVerify/{{ $user->verifyUser->token }}</a> or press the button below
         </p>
 
-        <div class="center">
-            <a role="button" class="verify-btn" href="{{ $domain }}/emailVerify/{{ $user->verifyUser->token }}">Verify my email</a>
+        <div style="text-align: center; margin-top: 28px;">
+            <a role="button" style="padding: 12px 30px;
+            border-radius: 30px;
+            background: #522e8e;
+            border: 3px solid #FFFFFF;
+            cursor: pointer;
+            color: #FFFFFF;
+            font-size: 20px;
+            text-align: center;
+            display: inline-block;
+            margin: 0 auto;
+            font-weight: bold;" href="{{ $domain }}/emailVerify/{{ $user->verifyUser->token }}">Verify my email</a>
         </div>
 
-        <p>
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 28px;">
             Verify email is the first step of the account validate process. For new account, after your email is verified we
-            wil proceed to complete your <a class="primary-text">Candidate</a> registration form.
+            wil proceed to complete your <a style="color: #522e8e;">Candidate</a> registration form.
         </p>
 
-        <p>
-            If you do not verify your email address by sent link within <span class="font-italic font-weight-bold text-black">30 minutes</span> then the sent
-            verification link & your signup informations will automatically unenrolled from Matrimony Assist platform, you can rejoin Matrimony Assist at any time by once again
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 28px;">
+            If you do not verify your email address by sent link within <span style="font-style: italic; font-weight: bold; color: #000000; opacity: 0.75;">30 minutes</span> then the sent
+            verification link & your signup information will automatically unenrolled from Matrimony Assist platform, you can rejoin Matrimony Assist at any time by once again
             completing the Matrimony Assist signup process.
         </p>
 
-        <p>
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 28px;">
             You're receiving this email because you recently created a new Matrimony Assist account or added a new email address. If this wasn't you,
             please ignore this email.
         </p>
 
-        <p class="mt-8">
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 32px">
             Thanks, <br>
             Regards <br>
             Matrimony Assist Team
         </p>
 
-        <p class="center mt-8">
-            This email was sent to <span class="primary-text">{{ $user['email'] }}</span>, which is
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 32px; text-align: center;">
+            This email was sent to <span style="color: #522e8e;">{{ $user['email'] }}</span>, which is
             associated with a Matrimony Assist account.
         </p>
 
-        <p class="center mt-5">
+        <p style="font-size: 16px; color: rgba(0,0,0,.5); margin-top: 20px; text-align: center;">
             &copy; 2022 Matrimony Assist Inc., All Rights Reserved Matrimony Assist Inc., 55 2nd Street London, UK 94105
         </p>
     </div>
