91ac86f24994bd68362beb1fd0ab21893a939d9b
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2022-02-17 00:04:54 +0000

    updated password service

--- routes/api.php
+++ routes/api.php
@@ -52,12 +52,7 @@ Route::POST('v1/login', [UserController::class, 'authenticate']);
 Route::GET('v1/logout', [UserController::class, 'logout']);//will be used  in save and exit
 //using ony get method
 Route::get('v1/emailVerify/{token}', [UserController::class, 'emailVerify']);//incomplete
-Route::get('v1/password-reset/{token}', static function ($token) {
-    $rule = ['token' => 'required|string|max:60'];
-    Validator::make(['token' => $token], $rule);
-    $tokenExistInDB = PasswordReset::where('token', $token)->exists();
-    return response()->json(['accepted_token' => $tokenExistInDB]);
-});
+Route::get('v1/password-reset/{token}', [UserController::class, 'passwordExpiryCheck']);
 Route::GET('v1/token-refresh', [UserController::class, 'getTokenRefresh']);//inspect
 Route::post('v1/forgot/password', ForgotPasswordController::class)->name('forgot.password');//inspect
 Route::post('v1/forgot/password/verify', [ForgotPasswordController::class, 'forgetPasswordTokenVerification'])->name('forgot.password.verify');//inspect
