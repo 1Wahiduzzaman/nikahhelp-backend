c3f5bf88dd1b99394aff49a828f28537d12e7340
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2022-06-30 10:45:52 +0100

    opting out from seeding public users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,20 +20,20 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('users')->truncate();
-        DB::table('candidate_information')->truncate();
+//        DB::table('users')->truncate();
+//        DB::table('candidate_information')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         //
-        $user = User::create([
-            'full_name' => 'Mr. User',
-            "is_verified" => 1,
-            "status" => "1",
-            'email' => 'user@mail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make(12345678),
-            'remember_token' => Str::random(10),
-            "account_type" => 1,
-        ]);
+//        $user = User::create([
+//            'full_name' => 'Mr. User',
+//            "is_verified" => 1,
+//            "status" => "1",
+//            'email' => 'user@mail.com',
+//            'email_verified_at' => now(),
+//            'password' => Hash::make(12345678),
+//            'remember_token' => Str::random(10),
+//            "account_type" => 1,
+//        ]);
         $superAdmin = Admin::create([
             'full_name' => 'Super Admin',
             "status" => "1",
@@ -64,42 +64,42 @@ class UserSeeder extends Seeder
         ]);
         $support->assignRole('Support');
 
-        CandidateInformation::create([
-            'user_id'=> $user->id,
-            'first_name'=> 'Mr',
-            'last_name'=> 'Admin',
-            'screen_name'=> 'AD5213',
-            'dob'=> '1988-10-28',
-            'per_gender'=>1,
-            "per_nationality" => 19,
-            'per_religion_id'=>1,
-            'data_input_status'=>6
-        ]);
-        $user1 = User::factory()->create();
-        $candidate1 = CandidateInformation::factory()->create([
-            'user_id'=>$user1->id,
-            "per_email" => $user1->email,
-            'anybody_can_see'=> 1,
-            'only_team_can_see'=> 0,
-            'team_connection_can_see'=> 0,
-        ]);
-
-        $user2 = User::factory()->create();
-        $candidate2 = CandidateInformation::factory()->create([
-            'user_id'=>$user2->id,
-            "per_email" => $user2->email,
-            'anybody_can_see'=> 0,
-            'only_team_can_see'=> 1,
-            'team_connection_can_see'=> 0,
-        ]);
-        $user3 = User::factory()->create();
-        $candidate3 = CandidateInformation::factory()->create([
-            'user_id'=>$user3->id,
-            "per_email" => $user3->email,
-            'anybody_can_see'=> 0,
-            'only_team_can_see'=> 0,
-            'team_connection_can_see'=> 1,
-        ]);
+//        CandidateInformation::create([
+//            'user_id'=> $user->id,
+//            'first_name'=> 'Mr',
+//            'last_name'=> 'Admin',
+//            'screen_name'=> 'AD5213',
+//            'dob'=> '1988-10-28',
+//            'per_gender'=>1,
+//            "per_nationality" => 19,
+//            'per_religion_id'=>1,
+//            'data_input_status'=>6
+//        ]);
+//        $user1 = User::factory()->create();
+//        $candidate1 = CandidateInformation::factory()->create([
+//            'user_id'=>$user1->id,
+//            "per_email" => $user1->email,
+//            'anybody_can_see'=> 1,
+//            'only_team_can_see'=> 0,
+//            'team_connection_can_see'=> 0,
+//        ]);
+//
+//        $user2 = User::factory()->create();
+//        $candidate2 = CandidateInformation::factory()->create([
+//            'user_id'=>$user2->id,
+//            "per_email" => $user2->email,
+//            'anybody_can_see'=> 0,
+//            'only_team_can_see'=> 1,
+//            'team_connection_can_see'=> 0,
+//        ]);
+//        $user3 = User::factory()->create();
+//        $candidate3 = CandidateInformation::factory()->create([
+//            'user_id'=>$user3->id,
+//            "per_email" => $user3->email,
+//            'anybody_can_see'=> 0,
+//            'only_team_can_see'=> 0,
+//            'team_connection_can_see'=> 1,
+//        ]);
 
 //        User::factory()->create()->each(function ($user){
 //            CandidateInformation::factory()->create([
