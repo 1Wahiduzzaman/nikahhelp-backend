9c377010a0af746acfb63ee90ece5be1f908ce30
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2022-07-06 15:46:02 +0100

    gender updated

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -328,7 +328,7 @@ class SearchService extends ApiBaseService
         try {
             $searchedCandidates = $this->candidateRepository->getModel()->with(['user' => function($query) use ($request) {
                 $query->where('status', '3');
-            }])->where('per_gender', $request->input('per_gender'))
+            }])->where('per_gender', (int)$request->input('per_gender'))
                 ->orWhere(function ($query) use ($request){
                     $query->where('per_religion_id', $request->input('religion'))
                         ->orWhere('per_employment_status', $request->input('employment_status'))
