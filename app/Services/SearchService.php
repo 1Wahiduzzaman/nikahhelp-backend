4f29b7423205f14592eb6cf8df1c2e23915e13a1
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2022-07-06 15:49:43 +0100

    updated request

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -324,11 +324,11 @@ class SearchService extends ApiBaseService
 
     public function searchCandidates(CandidateSearch $request)
     {
-
+        $gender = (int)$request->input('per_gender');
         try {
             $searchedCandidates = $this->candidateRepository->getModel()->with(['user' => function($query) use ($request) {
                 $query->where('status', '3');
-            }])->where('per_gender', (int)$request->input('per_gender'))
+            }])->where('per_gender', $gender)
                 ->orWhere(function ($query) use ($request){
                     $query->where('per_religion_id', $request->input('religion'))
                         ->orWhere('per_employment_status', $request->input('employment_status'))
