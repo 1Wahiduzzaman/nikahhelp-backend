7c50a824a8e1c724830af8548375bb0dddcd28cf
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2022-11-26 18:43:09 +0000

    change email and visit

--- app/Http/Controllers/API/V1/VisitController.php
+++ app/Http/Controllers/API/V1/VisitController.php
@@ -10,19 +10,19 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class VisitController extends Controller
-{    
+{
     public function visit(Request $request){
         $active_team_id = Generic::getActiveTeamId();
-        if($active_team_id!=$request->from_team_id || $active_team_id!=$request->to_team_id) {        
+        if($active_team_id!=$request->from_team_id || $active_team_id!=$request->to_team_id) {
             $conditions = [
                 'from_team_id' => $request->from_team_id,
                 'to_team_id' => $request->to_team_id,
                 'user_id' => Auth::id(),
-            ];            
+            ];
             $visit_count = Visit::where('from_team_id', $request->from_team_id)
             ->where('to_team_id', $request->to_team_id)
             ->where('user_id', Auth::id())
-            ->first();            
+            ->first();
             if(isset($visit_count->visit_count) && !empty($visit_count->visit_count)) {
                 $visit = $visit_count->visit_count + 1;
             } else {
@@ -33,35 +33,32 @@ class VisitController extends Controller
                 'to_team_id' => $request->to_team_id,
                 'user_id' => Auth::id(),
                 'visit_count' => $visit,
-                'lat' => $request->lat,
-                'lng' => $request->lng,
-                'country' => $request->country,
             ];
             Visit::updateOrCreate($conditions, $data);
             return $this->sendSuccessResponse([], 'Hit counted successfully');
-        }        
+        }
     }
 
-    public function visitGraph() {
-        $active_team_id = Generic::getActiveTeamId();
+    public function visitGraph(Request $request) {
         // $data = Visit::
         // select(["SUM(visit_count)", 'created_at', 'to_team_id'])
         // ->where('to_team_id', $active_team_id)
         // ->groupBy('created_at')
         // ->get();
-        $data = Visit::select(    
-            DB::raw("(sum(visit_count)) as view"),
-            // DB::raw("(DATE_FORMAT(created_at, '%d-%m-%Y')) as my_date")
-            DB::raw("(DATE_FORMAT(created_at, '%M')) as my_date")
-            )
-            ->orderBy(DB::raw("DATE_FORMAT(created_at, '%m')"))
-            // ->groupBy(DB::raw("DATE_FORMAT(created_at, '%d-%m-%Y')"))
-            ->groupBy(DB::raw("DATE_FORMAT(created_at, '%m')"))
-            ->get();
-            $result = [
-                'view' => $data->pluck('view')->toArray(),
-                'date' => $data->pluck('my_date')->toArray(),
-            ];
-            return $this->sendSuccessResponse($result, 'Hit counted successfully');
+         $visits =  Visit::where('from_team_id', $request->input('from_team_id'))->get();
+        // $data = Visit::select(
+        //     DB::raw("(sum(visit_count)) as view"),
+        //     // DB::raw("(DATE_FORMAT(created_at, '%d-%m-%Y')) as my_date")
+        //     DB::raw("(DATE_FORMAT(created_at, '%M')) as my_date")
+        //     )
+        //     ->orderBy(DB::raw("DATE_FORMAT(created_at, '%m')"))
+        //     // ->groupBy(DB::raw("DATE_FORMAT(created_at, '%d-%m-%Y')"))
+        //     ->groupBy(DB::raw("DATE_FORMAT(created_at, '%m')"))
+        //     ->get();
+        //     $result = [
+        //         'view' => $data->pluck('view')->toArray(),
+        //         'date' => $data->pluck('my_date')->toArray(),
+        //     ];
+            return $this->sendSuccessResponse($visits, 'Hit counted successfully');
     }
 }
