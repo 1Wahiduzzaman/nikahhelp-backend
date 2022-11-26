419701eefdc2ce9b03a451d95484ad02a527d70a
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2022-11-26 13:57:13 +0000

    update data

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -3,13 +3,13 @@
 
 </head>
 <body>
-    <h1>{{ $data->firstname }} has requested help</h1>
+    <h1>{{ $data['firstname'] }} has requested help</h1>
     <p>please read the message to help:</p>
     <p>
-        {{ $data->message }}
+        {{ $data['message'] }}
     </p>
 
-    <p>{{ $data->email }}</p>
-    <p>{{ $data->telephone }}</p>
+    <p>{{ $data['email'] }}</p>
+    <p>{{ $data['telephone'] }}</p>
 </body>
 </html>
