a0bd28f294391ea05adfc3fa1e9b7caf415590d4
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2023-05-27 14:50:20 +0100

    updated resources

---
+++ tests/Feature/PicturRequestTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class PicturRequestTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $client = new \GuzzleHttp\Client();
+
+        $res = $client->request('POST', 'https://chobi.arranzed.com/api/v1/register', [
+            'form_params' => [
+                'email' => 'justtolet@test.com',
+                'password' => 'hellowrld!23'
+            ]
+        ]);
+
+        $this->assertTrue($res->getStatusCode() == 200, 'success');
+        dd(json_decode($res->getBody()->getContents())->data->token->access_token);
+    }
+}
