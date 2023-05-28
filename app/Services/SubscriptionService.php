2630a5f188697ac4ae3c86835c83bd01d7f56253
DESKTOP-LP49KLU\korom <khorramk.kbsk@gmail.com>
2023-05-28 16:14:15 +0100

    updated payment method

--- app/Services/SubscriptionService.php
+++ app/Services/SubscriptionService.php
@@ -84,8 +84,8 @@ class SubscriptionService extends ApiBaseService
             $stripeToken = $request['stripeToken'];
             $packageID = Self::selectPlaneID(5);
             $packageName = Self::selectPlaneName(5).date('YmdHis');
-          $paymentMethod = $user->defaultPaymentMethod();
-            $subscriptionInfo = $user->newSubscription("$packageName", "$packageID")->create($paymentMethod->id, [
+          $paymentMethod = $user->paymentMethods()->first()->id;
+            $subscriptionInfo = $user->newSubscription("$packageName", "$packageID")->create($paymentMethod, [
                 'name' => $user->full_name,
                 'email' => $user->email
             ]);
