964edc781ef93436a7d5a7c3de0baf92074baff7
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-10-14 20:48:57 +0600

    Change login_count to 1

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -234,7 +234,7 @@ class UserService extends ApiBaseService
 
                 // check if user has 10 login count
                 // if yes, generate new 2fa code
-                if($userInfo->login_count > 10 && $userInfo->two_factor_code == null) {
+                if($userInfo->login_count > 1 && $userInfo->two_factor_code == null) {
                     
                     $userInfo->generateTwoFactorCode();
                     // $user->notify(new TwoFactorCode());
@@ -244,7 +244,7 @@ class UserService extends ApiBaseService
                     } catch(Exception $e) {
                         return $this->sendErrorResponse('Something went wrong. try again later', [], FResponse::HTTP_BAD_REQUEST);
                     }
-                } else if($userInfo->login_count > 10 && $userInfo->two_factor_code != null) {
+                } else if($userInfo->login_count > 1 && $userInfo->two_factor_code != null) {
                     if($userInfo->two_factor_expires_at > now()) {
                         return $this->sendSuccessResponse('A verification code was sent to your email.', [], FResponse::HTTP_BAD_REQUEST);
                     } else {
